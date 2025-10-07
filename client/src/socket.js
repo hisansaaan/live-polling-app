63820a9883e737eca05dd0421fb50808badb8ec4
hisansaaan <hisaaaaa.san@gmail.com>
2025-10-07 17:37:01 +0530

    Connect frontend to live backend

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,3 +1,3 @@
 import { io } from 'socket.io-client';
-const socket = io('https://live-polling-app-lp6q.onrender.com');
+const socket = io('https://live-polling-app-lp6q.onrender.com/');
 export default socket;
