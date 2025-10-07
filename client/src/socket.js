ad40fedc1d2cf553c4e935ef29e565701da95689
hisansaaan <hisaaaaa.san@gmail.com>
2025-10-07 18:14:01 +0530

    Update socket connection to live backend

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,3 +1,3 @@
 import { io } from 'socket.io-client';
-const socket = io('https://live-polling-app-lp6q.onrender.com/');
+const socket = io(import.meta.env.VITE_BACKEND_URL);
 export default socket;
